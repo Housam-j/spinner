b47c85386d3442b02cb89df5b00510cb980c41f3
Housam-j <housam-j@hotmail.com>
2021-11-02 19:35:08 +0000

    done

--- spinner2.js
+++ spinner2.js
@@ -1,28 +1,7 @@
-const arr=['|','/', '-','\\','|','/', '-','\\','|','/', '-','\\','|','/', '-','\\','|'];
+const arr = ['|','/', '-','\\','|','/', '-','\\','|','/', '-','\\','|','/', '-','\\','|'];
 let t = 100;
-for (let char of arr){
-setTimeout(() => {
-  process.stdout.write(`\r${char}`);
- }, t+=200)
+for (let char of arr) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}`);
+  }, t += 200);
 }
-
-
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 100);
- 
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   '); 
-// }, 700);
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 900);
